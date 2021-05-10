2d9c86c6c98a12d24a5f33ed17954b691d6c6400
Clay Risser <email@clayrisser.com>
2021-05-10 12:28:52 -0500

    Started logs

--- coupler/handlers.go
+++ coupler/handlers.go
@@ -1,6 +1,8 @@
 package coupler
 
 import (
+	"fmt"
+
 	integrationv1alpha2 "github.com/silicon-hills/integration-operator/api/v1alpha2"
 	"github.com/silicon-hills/integration-operator/services"
 )
@@ -15,21 +17,31 @@ func NewHandlers() *Handlers {
 	return &Handlers{s: services.NewServices()}
 }
 
-func (h *Handlers) HandlePlugCreated(plug *integrationv1alpha2.Plug) {}
+func (h *Handlers) HandlePlugCreated(plug *integrationv1alpha2.Plug) {
+	fmt.Println("plug created")
+}
 
 func (h *Handlers) HandleJoined(plug *integrationv1alpha2.Plug, socket *integrationv1alpha2.Socket, payload Payload) {
+	fmt.Println("joined")
 }
 
 func (h *Handlers) HandlePlugChanged(plug *integrationv1alpha2.Plug, socket *integrationv1alpha2.Socket, payload Payload) {
+	fmt.Println("plug changed")
 }
 
-func (h *Handlers) HandleSocketCreated(socket *integrationv1alpha2.Socket) {}
+func (h *Handlers) HandleSocketCreated(socket *integrationv1alpha2.Socket) {
+	fmt.Println("socket created")
+
+}
 
 func (h *Handlers) HandleSocketChanged(plug *integrationv1alpha2.Plug, socket *integrationv1alpha2.Socket, payload Payload) {
+	fmt.Println("socket changed")
 }
 
 func (h *Handlers) HandleDeparted(plug *integrationv1alpha2.Plug, socket *integrationv1alpha2.Socket, payload Payload) {
+	fmt.Println("departed")
 }
 
 func (h *Handlers) HandleBroken(plug *integrationv1alpha2.Plug, socket *integrationv1alpha2.Socket) {
+	fmt.Println("broken")
 }
