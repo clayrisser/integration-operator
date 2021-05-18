8711808ca7d1fd4e4f0980a1952426c911119394
Clay Risser <email@clayrisser.com>
2021-05-18 17:34:40 -0500

    Added created event

--- coupler/handlers.go
+++ coupler/handlers.go
@@ -1,8 +1,13 @@
 package coupler
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/go-resty/resty/v2"
+	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/json"
 	"github.com/tidwall/gjson"
 	"sigs.k8s.io/yaml"
 )
@@ -16,13 +21,37 @@ func NewHandlers() *Handlers {
 }
 
 func (h *Handlers) HandlePlugCreated(plug gjson.Result) error {
-	fmt.Println("plug created")
-	y, err := yaml.JSONToYAML([]byte(plug.String()))
+	m := minify.New()
+	m.AddFunc("application/json", json.Minify)
+	endpoint := plug.Get("spec.configEndpoint").String()
+	client := resty.New()
+	rCh := make(chan *resty.Response)
+	errCh := make(chan error)
+	body, err := m.String("application/json", fmt.Sprintf(`{
+  "version": "1",
+  "plug": %s
+}`, plug.String()))
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(y))
-	return nil
+	go func() {
+		r, err := client.R().EnableTrace().SetHeaders(map[string]string{
+			"Content-Type": "application/json",
+		}).SetBody([]byte(body)).Post(endpoint)
+		if err != nil {
+			errCh <- err
+		}
+		rCh <- r
+	}()
+	select {
+	case r := <-rCh:
+		fmt.Printf(string(r.Body()))
+		return nil
+	case err := <-errCh:
+		return err
+	case <-time.After(3 * time.Second):
+		return errors.New("timeout")
+	}
 }
 
 func (h *Handlers) HandlePlugJoined(plug gjson.Result, socket gjson.Result, config gjson.Result) error {
@@ -69,4 +98,11 @@ func (h *Handlers) HandleDeparted(plug gjson.Result, socket gjson.Result) error 
 func (h *Handlers) HandleBroken() error {
 	fmt.Println("broken")
 	return nil
+}
+
+type CouplerBody struct {
+	Config  gjson.Result `json:"config,omitempty"`
+	Plug    gjson.Result `json:"plug,omitempty"`
+	Socket  gjson.Result `json:"socket,omitempty"`
+	Version string       `json:"version,omitempty"`
 }
