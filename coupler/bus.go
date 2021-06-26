d83d2b19475b9d773518b00847edced49a13b1db
Clay Risser <email@clayrisser.com>
2021-06-26 10:56:16 -0500

    Licensed project

--- coupler/bus.go
+++ coupler/bus.go
@@ -1,6 +1,28 @@
-/**
- * inspired by https://levelup.gitconnected.com/lets-write-a-simple-event-bus-in-go-79b9480d8997
+/*
+ * File: /coupler/bus.go
+ * Project: integration-operator
+ * File Created: 23-06-2021 09:14:26
+ * Author: Clay Risser <email@clayrisser.com>
+ * -----
+ * Last Modified: 26-06-2021 10:54:56
+ * Modified By: Clay Risser <email@clayrisser.com>
+ * -----
+ * Silicon Hills LLC (c) Copyright 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+// inspired by https://levelup.gitconnected.com/lets-write-a-simple-event-bus-in-go-79b9480d8997
 
 package coupler
 
--- util/data.go
+++ util/data.go
@@ -1,3 +1,27 @@
+/*
+ * File: /util/data.go
+ * Project: integration-operator
+ * File Created: 23-06-2021 22:11:01
+ * Author: Clay Risser <email@clayrisser.com>
+ * -----
+ * Last Modified: 26-06-2021 10:53:21
+ * Modified By: Clay Risser <email@clayrisser.com>
+ * -----
+ * Silicon Hills LLC (c) Copyright 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package util
 
 import (
@@ -18,6 +42,7 @@ func NewDataUtil(ctx *context.Context) *DataUtil {
 	return &DataUtil{
 		client: kubernetes.NewForConfigOrDie(ctrl.GetConfigOrDie()),
 	}
+
 }
 
 func (u *DataUtil) GetPlugData(plug *integrationv1alpha2.Plug) (map[string]string, error) {
