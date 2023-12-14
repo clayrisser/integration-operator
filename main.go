c4e7a0e2ff4e5b64f80f4a1a6c5cd318bd73bbb7
Clay Risser <clayrisser@gmail.com>
2023-12-14 01:38:15 -0600

    Add RBAC rules for configmaps, secrets, pods, and services

--- main.go
+++ main.go
@@ -61,6 +61,8 @@ func init() {
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=impersonate
+//+kubebuilder:rbac:groups="",resources=services;pods,verbs=create;delete;get;list;patch;update;watch
+//+kubebuilder:rbac:groups="",resources=configmaps;secrets,verbs=get;list;watch
 
 func main() {
 	var enableLeaderElection bool
